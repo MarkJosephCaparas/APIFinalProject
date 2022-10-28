e443e15f6f708403777c4f25ea1c6545a52cd624
Mark Joseph Caparas <markjoseph.caparas@ph.softvision.global>
2022-10-28 19:51:34 +0800

    SOAP UPDATE

---
+++ ProjectHTTPClient/ProjectREST/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ProjectREST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ProjectHTTPClient/ProjectSOAP/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ProjectSOAP
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
